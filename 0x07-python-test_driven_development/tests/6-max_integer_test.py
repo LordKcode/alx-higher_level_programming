6f827d58464cf4c1da360d8df8b2d0c5a696cf90
LordKcode <101455308+LordKcode@users.noreply.github.com>
2022-09-21 04:38:47 -0700

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,28 +6,41 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """ tests for max_integer
-    """
 
-    def test_max_int_basic(self):
-        """ tests normal list of ints
-        """
+    def test_valid(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
-    def test_max_int_empty(self):
-        """ tests if list is empty
-        """
+    def test_negative(self):
+        self.assertEqual(max_integer([1, -1, 0, 2]), 2)
+        self.assertEqual(max_integer([-1, -1, 0]), 0)
+
+    def test_empty(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_max_int_neg(self):
-        """ tests if list has a negative int
-        """
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+    def test_same(self):
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([-1, -1, -1, -1]), -1)
 
-    def test_max_int_one(self):
-        """ tests if list has only one item
-        """
+    def test_type(self):
+        self.assertIs(list, list)
+
+    def test_twoMax(self):
+        self.assertEqual(max_integer([2, 2, 0, -1]), 2)
+        self.assertEqual(max_integer([-1, 0, 0, -1]), 0)
+
+    def test_oneArg(self):
         self.assertEqual(max_integer([1]), 1)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_floats(self):
+        self.assertEqual(max_integer([0.0, 0, 1]), 1)
+        self.assertEqual(max_integer([1.0, 0, 0.0]), 1)
+
+    def test_invalid(self):
+        self.assertRaises(TypeError, max_integer, [1, "cat", 3],
+                          msg="unorderable types: str() > int()")
+        self.assertRaises(TypeError, max_integer, [1, 0, "7"],
+                          msg="unorderable types: str() > int()")
+
+    def test_weird(self):
+        self.assertEqual(max_integer([1, 0, 00]), 1)
