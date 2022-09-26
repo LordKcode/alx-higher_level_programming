8a279e410e957dae4836151f61138ac9d5aec6a0
LordKcode <101455308+LordKcode@users.noreply.github.com>
2022-09-26 12:01:17 -0700

    Create 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+"""
+Square class module.
+
+Define Square class.
+"""
+
+
+class Square(Rectangle):
+    """Define a Square."""
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[{}] {}/{}".format(type(self).__name__, self.__size,
+                                   self.__size)
