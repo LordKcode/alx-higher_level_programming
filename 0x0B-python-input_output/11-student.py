f39ce994102403b7ee9164e250116a8eef0db2b7
LordKcode <101455308+LordKcode@users.noreply.github.com>
2022-09-28 11:19:07 -0700

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python3
 """
-Contains the clas "Student"
+a class Student
 """
 
 
 class Student:
-    """Representation of a student"""
+    """
+    Definition of the Student class
+    """
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """
+        Instantiation of the class with:
+        first_name
+        last_name
+        age
+        """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """returns a dictionary representation of a Student instance"""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation
+        of a Student instance:
+            If attrs is a list of strings, only attribute names
+            contained in this list must be retrieved.
+            Otherwise, all attributes must be retrieved
+        """
+        if type(attrs) == list:
+            if all(type(elem) == str for elem in attrs):
+                return {key: getattr(self, key)
+                        for key in attrs if hasattr(self, key)}
+        class_dict = self.__dict__
+        return class_dict
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance:
+            You can assume json will always be a dictionary
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
