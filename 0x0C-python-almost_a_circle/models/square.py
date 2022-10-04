a4c5ed9385521ad60bd39b03d3b576078eb1ebf1
LordKcode <101455308+LordKcode@users.noreply.github.com>
2022-10-03 18:24:03 -0700

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,45 @@
+#!/usr/bin/python3
+""" Square class module
+"""
+from models.rectangle import Rectangle
 
+
+class Square(Rectangle):
+    """ Square class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Updates the attributes with the given values."""
+        if args is not None and len(args) > 0:
+            self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        elif kwargs is not None:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns the dictionary version of self. """
+        return dict(id=self.id, size=self.size, x=self.x, y=self.y)
